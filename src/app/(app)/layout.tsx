bf3f833eb067e1ae5324e22a2df280bc76451c7b
Ian Mwinga <ianmwinga254@gmail.com>
2025-10-17 07:30:49 +0000

    okay build a login page  that can work with supabase

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -101,8 +101,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         </SidebarFooter>
       </Sidebar>
       <SidebarInset>
-        <AppHeader />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+        <main className="flex-1 overflow-y-auto">
           {children}
         </main>
       </SidebarInset>
